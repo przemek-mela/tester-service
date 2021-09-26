c30bf8d77491bf5e7bd99c52e5f9788e3c5d8029
Przemek Mela <develop4@fa1.pl>
2021-09-26 21:31:24 +0200

    Added crossOrigin for development communication, Added method for search request

--- src/main/java/com/tester/controller/BugController.java
+++ src/main/java/com/tester/controller/BugController.java
@@ -1,17 +1,28 @@
 package com.tester.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.tester.service.BugService;
+import com.tester.pojo.SearchResult;
+import com.tester.repository.BugDao;
 
+@CrossOrigin(origins = "http://localhost:4220")
 @RestController
 @RequestMapping("/bug")
 public class BugController {
 
     @Autowired
-    BugService bugService;
-    
+    BugDao bugDao;
+
+    @GetMapping("/search")
+    public List<SearchResult> getBugs(@RequestParam("countries") Optional<List<String>> countries, @RequestParam("devices") Optional<List<Integer>> devices) {
+	return bugDao.getBugsSearchResults(countries, devices);
+    }
 }
