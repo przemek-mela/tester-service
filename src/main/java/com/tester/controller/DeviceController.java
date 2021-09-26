c3a0be07ec25ec07f43e3f6134e9775d912056ca
Przemek Mela <develop4@fa1.pl>
2021-09-26 21:29:19 +0200

    Added crossOrigin to comunicate on development

--- src/main/java/com/tester/controller/DeviceController.java
+++ src/main/java/com/tester/controller/DeviceController.java
@@ -4,6 +4,7 @@ package com.tester.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.tester.models.Device;
 import com.tester.repository.DeviceDao;
 
+@CrossOrigin(origins = "http://localhost:4220")
 @RestController
 @RequestMapping("/device")
 public class DeviceController {
@@ -18,7 +20,7 @@ public class DeviceController {
     @Autowired
     DeviceDao deviceDao;
     
-    @GetMapping("/")
+    @GetMapping("")
     public List<Device> getAllDevices(){
 	return deviceDao.findAll();
     }
