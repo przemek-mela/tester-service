382a5a4ed62269a263e93d393af1f3e5196982f4
Przemek Mela <develop4@fa1.pl>
2021-09-26 21:31:52 +0200

    Added crossOrigin for development communication

--- src/main/java/com/tester/controller/TesterController.java
+++ src/main/java/com/tester/controller/TesterController.java
@@ -3,12 +3,14 @@ package com.tester.controller;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tester.service.TesterService;
 
+@CrossOrigin(origins = "http://localhost:4220")
 @RestController
 @RequestMapping("/tester")
 public class TesterController {
